9ec79376d207394780906a5e9f560cf7dae5e32a
adnanmulalic <adnan.mulalic3@gmail.com>
2022-08-16 10:54:29 +0200

    Exercise passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+  let newText = "";
+  let i = 0;
+  while (i < num) {
+    newText += text;
+    i++;
+  }
+  return newText;
 };
 
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
