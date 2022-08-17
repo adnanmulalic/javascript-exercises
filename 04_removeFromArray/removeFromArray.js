b34754f2fd4362d022d9b666fced85d7d829a6a9
adnanmulalic <adnan.mulalic3@gmail.com>
2022-08-17 12:27:40 +0200

    First test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(firstArray, numb) {
+    const newArray = [];
+    firstArray.forEach(element => {
+        if (element != numb) {
+            newArray.push(element);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
