28b1724e9afd299d49447c40f09bcf1bfdcd1e2e
adnanmulalic <adnan.mulalic3@gmail.com>
2022-08-18 11:43:45 +0200

    wip

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-const removeFromArray = function(firstArray, numb) {
+/* const removeFromArray = function(firstArray, numb) {
     const newArray = [];
     firstArray.forEach(element => {
         if (element != numb) {
@@ -6,6 +6,22 @@ const removeFromArray = function(firstArray, numb) {
         }
     });
     return newArray;
+}; */
+
+const removeFromArray = function(firstArray, ...args) {
+        let newArray = [];
+        const argsArray = args;
+        firstArray.forEach(element => {
+            for (let i = 0; i < argsArray.length; i++) {
+                console.log(argsArray[i]);
+                newArray.push(element);
+                if (element === argsArray[i]) {
+                    newArray.pop(element);
+                }
+            }
+        });
+    console.log(newArray);
+    
 };
 
 // Do not edit below this line
