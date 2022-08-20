bbb38b7c5e95ead9db9004a64e88a3e00cc455c4
adnanmulalic <adnan.mulalic3@gmail.com>
2022-08-20 11:38:24 +0200

    Exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) && a > 0 && Number.isInteger(b) && b > 0) {
+        if (a > b) {
+            let c = a;
+            a = b;
+            b = c;
+        }
+        let sum = 0;
+        for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
