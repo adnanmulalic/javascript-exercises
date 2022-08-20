2ad4ba4d0eeb2182ae68e2f5a6878417ee9bb428
adnanmulalic <adnan.mulalic3@gmail.com>
2022-08-20 09:45:23 +0200

    Works

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,35 +1,15 @@
-/* const removeFromArray = function(firstArray, numb) {
-    const newArray = [];
-    firstArray.forEach(element => {
-        if (element != numb) {
-            newArray.push(element);
-        }
-    });
-    return newArray;
-}; */
-
 const removeFromArray = function(firstArray, ...args) {
-        let newArray = [];
-        const argsArray = Array.from(args);
-        firstArray.forEach(element => {
-            console.log(firstArray.indexOf(element));
-            for (let i = 0; i < argsArray.length; i++) {
-                console.log(argsArray[i]);
-                if (element === argsArray[i]) {
-                    newArray.push(element);
-                }
+    const argsArray = args;
+    //argsArray.sort();
+    console.log(args);
+    for (let i = 0; i < argsArray.length; i++) {
+        for (let y = 0; y < firstArray.length; y++) {
+            if (argsArray[i] === firstArray[y]) {
+                firstArray.splice(firstArray.indexOf(firstArray[y]), 1);
             }
-        });
-    console.log(newArray);
-    let finalArray = [];
-    for (let i = 0; i < firstArray.length; i++) {
-        for (let y = 0; y < newArray.length; y++) {
-            if (firstArray[i] != newArray[y]) {
-                finalArray.push(firstArray[i]);
-            }
-        }       
+        }  
     }
-    return finalArray;
+return firstArray;
 };
 
 // Do not edit below this line
