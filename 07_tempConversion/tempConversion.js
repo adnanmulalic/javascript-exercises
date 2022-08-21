059e96e82c4ebe70dfe1de52e042e95544e4df38
adnanmulalic <adnan.mulalic3@gmail.com>
2022-08-21 11:06:27 +0200

    Exercise done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let c = ((fahrenheit - 32) * (5 / 9)).toFixed(1);
+  return parseFloat(c);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let f = (celsius * (9 / 5) + 32).toFixed(1);
+  return parseFloat(f);
 };
 
 // Do not edit below this line
