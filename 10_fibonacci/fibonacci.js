73d593703f04d9a0e5eceefd1c8b1a5b51d575d1
adnanmulalic <adnan.mulalic3@gmail.com>
2022-09-03 11:40:02 +0200

    Exercise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    const fibNums = [];
+    let firstNumber = 0;
+    let secondNumber = 1;
+    let sum = 1;
+    for (let i = 0; i < num - 1; i++) {
+        sum = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = sum;
+  }
+  return sum;
 };
 
 // Do not edit below this line
