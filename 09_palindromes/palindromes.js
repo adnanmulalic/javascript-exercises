b1c224ab1ee9c0ee0d70911473b2654f32c4af66
adnanmulalic <adnan.mulalic3@gmail.com>
2022-09-03 11:02:37 +0200

    Exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,33 @@
+const puncMarks = [".",",",":",";","?","!","...",'"',"'","/"," "];
 const palindromes = function (text) {
-    //let pattern = /[a-zA-Z]/g;
-    let reverseText = "";
-    for (let i = text.length - 1; i >= 0; i--) {
-        reverseText += text[i];        
+    text = text.toLowerCase();
+    let textArray = [];
+    for (let i = 0; i < text.length; i++) {
+        textArray.push(text[i]);   
     }
-    if (reverseText === text) {
-        console.log(text, "<==>", reverseText);
+    let cleanText = "";
+    for (let i = 0; i < textArray.length; i++) {
+        let hasPuncMark = false;
+        for (let j = 0; j < puncMarks.length; j++) {
+            if (textArray[i] === puncMarks[j]) {
+                hasPuncMark = true;
+            }
+        }
+        if (hasPuncMark === false) {
+            cleanText += textArray[i];
+        }   
+    }
+    let reverseText = "";
+    for (let i = cleanText.length - 1; i >= 0; i--) {
+        reverseText += cleanText[i];        
+    }
+    if (cleanText === reverseText) {
+        console.log(cleanText, "<==>", reverseText);
         return true;
     } else {
-        console.log(text, "<==>", reverseText);
+        console.log(cleanText, "<==>", reverseText);
         return false;
     }
-
 };
 
 // Do not edit below this line
