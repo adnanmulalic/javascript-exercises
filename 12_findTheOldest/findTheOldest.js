3910daa87ab7174e320e62224b84eaf8b63f5ca3
adnanmulalic <adnan.mulalic3@gmail.com>
2022-09-04 11:48:45 +0200

    First test passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ageNameArray = people.map((person) => {
+        return {
+            name: person.name,
+            age: person.yearOfDeath - person.yearOfBirth,
+        };
+    });
+    let currentOldest = ageNameArray[0].age;
+    const oldestPerson = ageNameArray.filter((person) => {
+        if (person.age > currentOldest) {
+            currentOldest = person.age;
+            return person;
+        }
+    })
+    if (oldestPerson[0] === undefined) {
+        return ageNameArray[0];
+    } else {    
+    return oldestPerson[0];
+    }
 };
 
 // Do not edit below this line
